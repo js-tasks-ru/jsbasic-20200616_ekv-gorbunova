434ad9e9fa1e021087965579b374fbf51360fe44
Ekaterina <ekv.gorbunova@gmail.com>
2020-06-23 13:08:54 +0200

    удалила console.log

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -7,6 +7,4 @@ function ucFirst(str) {
   if (!str) return str;
   return str[0].toUpperCase() + str.slice(1);
 }
-console.log(ucFirst('вася'));
-console.log(ucFirst('в'));
-console.log(ucFirst(''));
+
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -7,6 +7,4 @@ function checkSpam(str) {
   let lowerStr = str.toLowerCase();
   return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
-console.log(checkSpam('1XbeT now'));
-console.log(checkSpam('free xxxxx'));
-console.log(checkSpam('innocent rabbit'));
+
