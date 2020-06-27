8c8bd49c7ec8fddf3ce11ecb413abbdabe78c6e1
Ekaterina <ekv.gorbunova@gmail.com>
2020-06-27 12:09:49 +0200

    решение 3й здачи из второго урока

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  read: function (a, b) {
+    this.c = a;
+    this.d = b;
+  },
+  sum: function () {
+    return this.c + this.d
+  },
+  mul: function () {
+    return this.c * this.d
+  },
+  c: 0,
+  d: 0,
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
